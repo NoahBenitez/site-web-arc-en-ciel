dc5cce40ef89c84a793c3bf6cad5d85d7a57bab3
NoahBenitez <148209436+NoahBenitez@users.noreply.github.com>
2024-11-05 18:17:37 +0100

    Add files via upload

---
+++ Projet site de nouille/html/fichier.js
@@ -0,0 +1,14 @@
+// Récupérer les éléments <a> par leur ID
+var Accueilhtml = document.getElementById("Accueil.html");
+var link2 = document.getElementById("link2");
+var link3 = document.getElementById("link3");
+
+// Définir les URLs dynamiques
+var url1 = "https://Accueil.html/Accueil";
+var url2 = "https://exemple.com/page2";
+var url3 = "https://exemple.com/page3";
+
+// Mettre à jour les attributs href avec les URLs dynamiques
+link1.href = url1;
+link2.href = url2;
+link3.href = url3;
